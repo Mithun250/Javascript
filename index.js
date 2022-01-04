a37bdc3fc6bf3ed66a48ef74fd21e77476a81acb
Mithun250 <83149908+Mithun250@users.noreply.github.com>
2022-01-04 11:11:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+let name='Mithun';
+console.log(name);
+
+//constant
+  const interestRate=0.3;
+  console.log(interestRate);
+
+
+
+//object
+let person={
+    name:'Mithun',
+    age:20
+};
+
+person.name='joe';
+
+console.log(person.name);
+
+//array
+let selectedColors=['red','blue'];
+ selectedColors[2]=1;
+console.log(selectedColors);
+
+//fuction
+
+//performing a task
+function me(name)
+{
+    console.log('Hello ' + name);
+}
+
+me('mithun');
+
+//calculate the value
+function square(number){
+    return number*number;
+}
+console.log(square(2));
